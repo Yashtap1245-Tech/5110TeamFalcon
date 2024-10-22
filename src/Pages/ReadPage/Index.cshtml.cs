bc2f2b6170edff27961775194e6f84e7675af5bd
Vamshi-toramamidi <toramamidivamshi@gmail.com>
2024-10-22 15:36:43 -0700

    Linked the ReadModel and Json file in Index.cshtml.cs

--- src/Pages/ReadPage/Index.cshtml.cs
+++ src/Pages/ReadPage/Index.cshtml.cs
@@ -10,22 +10,22 @@ namespace ContosoCrafts.WebSite.Pages.ReadPage
     public class ReadPageModel : PageModel
     {
         // Data middletier
-        public JsonFileProductService ProductService { get; }
+        public JsonFileReadService ProductService { get; }
         // public string MovieTitle { get; set; }
         // public string MoviePosterUrl { get; set; }
         // public string Description { get; set; }
         // public string CaseDetails { get; set; }
         // public double Rating { get; set; } // Use float or decimal if preferred
         
-        public ReadPageModel(JsonFileProductService productService) {
+        public ReadPageModel(JsonFileReadService productService) {
             ProductService = productService;
         }
         
         // public IEnumerable<ProductModel> Products { get; private set; }
         // The data to show
         // public ProductModel Product;
-        public ProductModel Product;
-
+        public ReadModel Product;
+       
         public void OnGet(string id)
         {
             Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
