8007aeeac56723e8286907f3390f9aa51fb1a1d7
Sai Sreyas Sistla <ssistla@seattleu.edu>
2024-10-27 14:43:30 -0700

    Added comment above the Url Field in ProductModel.cs file

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -15,7 +15,7 @@ namespace ContosoCrafts.WebSite.Models
         //Image Field
         [JsonPropertyName("img")]
         public string Image { get; set; }
-
+        //Url Field
         public string Url { get; set; }
         
         [StringLength (maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
