87410e620c68f248c9b69be58d13b7d8a2a87fe3
Sai Sreyas Sistla <ssistla@seattleu.edu>
2024-10-27 17:19:20 -0700

    Added comment in the Rating attribute for class "RatingRequest" in ProductsController.cs file

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -34,7 +34,9 @@ namespace ContosoCrafts.WebSite.Controllers
 
         public class RatingRequest
         {
+            
             public string ProductId { get; set; }
+            //Rating Field
             public int Rating { get; set; }
         }
     }
