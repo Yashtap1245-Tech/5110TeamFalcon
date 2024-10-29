e670250f6bf0f6e8cd05efa12bd544b881faf0cf
Mehant Kumar <mehant98@gmail.com>
2024-10-29 16:37:51 -0700

    Added test for Update page in Product/Update.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Update.cshtml.Tests.cs
@@ -0,0 +1,128 @@
+using System.Collections.Generic;
+using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using ContosoCrafts.WebSite.Pages.Product;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests.Pages.Product.Update
+{
+    public class UpdateTests
+    {
+        #region TestSetup
+
+        public static IUrlHelperFactory urlHelperFactory;
+        public static DefaultHttpContext httpContextDefault;
+        public static IWebHostEnvironment webHostEnvironment;
+        public static ModelStateDictionary modelState;
+        public static ActionContext actionContext;
+        public static EmptyModelMetadataProvider modelMetadataProvider;
+        public static ViewDataDictionary viewData;
+        public static TempDataDictionary tempData;
+        public static PageContext pageContext;
+
+        public static UpdateModel PageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            httpContextDefault = new DefaultHttpContext()
+            {
+                //RequestServices = serviceProviderMock.Object,
+            };
+
+            modelState = new ModelStateDictionary();
+
+            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
+
+            modelMetadataProvider = new EmptyModelMetadataProvider();
+            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
+
+            pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData,
+            };
+
+            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net7.0/wwwroot");
+            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+
+            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
+            JsonFileProductService productService;
+
+            productService = new JsonFileProductService(mockWebHostEnvironment.Object);
+
+            PageModel = new UpdateModel(productService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+        
+        [Test]
+        public void OnGet_Valid_Should_Return_Product()
+        {
+            // Arrange
+            
+            // Act
+            PageModel.OnGet("jenlooper-cactus");
+            
+            Assert.AreEqual(true, PageModel.ModelState.IsValid);
+            Assert.AreEqual("The Shawshank Redemption", PageModel.Product.Title);
+            Assert.AreEqual("The Shawshank Redemption is a 1994 American prison drama film written and directed by Frank Darabont, based on the 1982 Stephen King novella Rita Hayworth and Shawshank Redemption. The film tells the story of banker Andy Dufresne (Tim Robbins), who is sentenced to life in Shawshank State Penitentiary for the murders of his wife and her lover, despite his claims of innocence. Over the following two decades, he befriends a fellow prisoner, contraband smuggler Ellis Red Redding (Morgan Freeman), and becomes instrumental in a money laundering operation led by the prison warden Samuel Norton (Bob Gunton). William Sadler, Clancy Brown, Gil Bellows, and James Whitmore appear in supporting roles.", PageModel.Product.Description);
+            Assert.AreEqual("Frank Darabont", PageModel.Product.Director);
+            Assert.AreEqual("Drama", PageModel.Product.Genre);
+        }
+
+        [Test]
+        public void OnPost_InValid_Model_NotValid_Return_Page()
+        {
+            // Force an invalid error state
+            PageModel.ModelState.AddModelError("bogus", "Bogus Error");
+            
+            // Act
+            var result = PageModel.OnPost() as ActionResult;
+            
+            // Assert
+            Assert.AreEqual(false, PageModel.ModelState.IsValid);
+        }
+
+        [Test]
+        public void OnPost_Valid_Should_Return_Products()
+        {
+            // Arrange
+            PageModel.Product = new ProductModel
+            {
+                Id = "jenlooper-cactus",
+                Title = "The Shawshank Redemption",
+                Description =
+                    "The Shawshank Redemption is a 1994 American prison drama film written and directed by Frank Darabont, based on the 1982 Stephen King novella Rita Hayworth and Shawshank Redemption. The film tells the story of banker Andy Dufresne (Tim Robbins), who is sentenced to life in Shawshank State Penitentiary for the murders of his wife and her lover, despite his claims of innocence. Over the following two decades, he befriends a fellow prisoner, contraband smuggler Ellis Red Redding (Morgan Freeman), and becomes instrumental in a money laundering operation led by the prison warden Samuel Norton (Bob Gunton). William Sadler, Clancy Brown, Gil Bellows, and James Whitmore appear in supporting roles.",
+                Director = "Frank Darabont",
+                Image = "The Shawshank Redemption",
+                Genre = "Drama",
+                YouTubeID = "NmzuHjWmXOc",
+            };
+            
+            // Act
+            var result = PageModel.OnPost() as RedirectToPageResult;
+            
+            // Assert
+            Assert.AreEqual(true, PageModel.ModelState.IsValid);
+            Assert.AreEqual(true, result.PageName.Contains("Index"));
+            
+
+        }
+    }
+}
