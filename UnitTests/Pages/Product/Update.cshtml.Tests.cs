ab0c01eed8ee7b0aadb957b32be254c6df99d372
Mehant Kumar <mehant98@gmail.com>
2024-10-29 19:13:19 -0700

    Updated Assert.AreEqual to Asert.This in Product/Update.cshtml.Tests.cs

--- UnitTests/Pages/Product/Update.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Update.cshtml.Tests.cs
@@ -79,11 +79,16 @@ namespace UnitTests.Pages.Product.Update
             // Act
             PageModel.OnGet("jenlooper-cactus");
             
-            Assert.AreEqual(true, PageModel.ModelState.IsValid);
-            Assert.AreEqual("The Shawshank Redemption", PageModel.Product.Title);
-            Assert.AreEqual("The Shawshank Redemption is a 1994 American prison drama film written and directed by Frank Darabont, based on the 1982 Stephen King novella Rita Hayworth and Shawshank Redemption. The film tells the story of banker Andy Dufresne (Tim Robbins), who is sentenced to life in Shawshank State Penitentiary for the murders of his wife and her lover, despite his claims of innocence. Over the following two decades, he befriends a fellow prisoner, contraband smuggler Ellis Red Redding (Morgan Freeman), and becomes instrumental in a money laundering operation led by the prison warden Samuel Norton (Bob Gunton). William Sadler, Clancy Brown, Gil Bellows, and James Whitmore appear in supporting roles.", PageModel.Product.Description);
-            Assert.AreEqual("Frank Darabont", PageModel.Product.Director);
-            Assert.AreEqual("Drama", PageModel.Product.Genre);
+            // Assert.AreEqual(true, PageModel.ModelState.IsValid);
+            // Assert.AreEqual("The Shawshank Redemption", PageModel.Product.Title);
+            // Assert.AreEqual("", PageModel.Product.Description);
+            // Assert.AreEqual("Frank Darabont", PageModel.Product.Director);
+            // Assert.AreEqual("Drama", PageModel.Product.Genre);
+            Assert.That(PageModel.ModelState.IsValid, Is.EqualTo(true));
+            Assert.That(PageModel.Product.Title, Is.EqualTo("The Shawshank Redemption"));
+            Assert.That(PageModel.Product.Director, Is.EqualTo("Frank Darabont"));
+            Assert.That(PageModel.Product.Description, Is.EqualTo("The Shawshank Redemption is a 1994 American prison drama film written and directed by Frank Darabont, based on the 1982 Stephen King novella Rita Hayworth and Shawshank Redemption. The film tells the story of banker Andy Dufresne (Tim Robbins), who is sentenced to life in Shawshank State Penitentiary for the murders of his wife and her lover, despite his claims of innocence. Over the following two decades, he befriends a fellow prisoner, contraband smuggler Ellis Red Redding (Morgan Freeman), and becomes instrumental in a money laundering operation led by the prison warden Samuel Norton (Bob Gunton). William Sadler, Clancy Brown, Gil Bellows, and James Whitmore appear in supporting roles."));
+            Assert.That(PageModel.Product.Genre, Is.EqualTo("Drama"));
         }
 
         [Test]
@@ -96,7 +101,8 @@ namespace UnitTests.Pages.Product.Update
             var result = PageModel.OnPost() as ActionResult;
             
             // Assert
-            Assert.AreEqual(false, PageModel.ModelState.IsValid);
+            // Assert.AreEqual(false, PageModel.ModelState.IsValid);
+            Assert.That(PageModel.ModelState.IsValid, Is.EqualTo(false));
         }
 
         [Test]
@@ -119,9 +125,10 @@ namespace UnitTests.Pages.Product.Update
             var result = PageModel.OnPost() as RedirectToPageResult;
             
             // Assert
-            Assert.AreEqual(true, PageModel.ModelState.IsValid);
-            Assert.AreEqual(true, result.PageName.Contains("Index"));
-            
+            // Assert.AreEqual(true, PageModel.ModelState.IsValid);
+            // Assert.AreEqual(true, result.PageName.Contains("Index"));
+            Assert.That(PageModel.ModelState.IsValid, Is.EqualTo(true));            
+            Assert.That(result.PageName.Contains("Index"), Is.EqualTo(true));            
 
         }
     }
