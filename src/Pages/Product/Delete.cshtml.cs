54659f0e17520745f6f4cb672bb3e08ecdf85b4d
Sai Sreyas Sistla <ssistla@seattleu.edu>
2024-11-01 20:06:18 -0700

    Added code functionality for delete product operation in Delete.cshtml.cs file

--- src/Pages/Product/Delete.cshtml.cs
+++ src/Pages/Product/Delete.cshtml.cs
@@ -1,12 +1,36 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+
+        public DeleteModel(JsonFileProductService productService)
         {
+            ProductService = productService;
+        }
+        // Collection of the Data
+        [BindProperty]
+        public ProductModel Product { get; set; }
+        public void OnGet(string id)
+        {
+            // Product = ProductService.GetDataForRead(id);
+            Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            ProductService.UpdateData(Product);
+            return RedirectToPage("./Index");
         }
     }
 }
