32a2cf01bc792d5626903b9070476880c6027351
Mehant Kumar <mehant98@gmail.com>
2024-11-03 08:44:02 -0800

    Added missing comments in Pages/Error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -9,17 +9,27 @@ namespace ContosoCrafts.WebSite.Pages
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
+        // Property to hold the unique request identifier.
         public string RequestId { get; set; }
 
+        // Property that indicates whether to show the RequestId.
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
+        
+        // Logger instance for logging error information.
         private readonly ILogger<ErrorModel> _logger;
 
+        /// <summary>
+        /// Constructor initializing the ErrorModel with a logger for logging errors.
+        /// </summary>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// REST OnGet
+        /// Return all the data
+        /// </summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
