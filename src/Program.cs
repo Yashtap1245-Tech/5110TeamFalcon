96944d64f6aab9f7b7c262f38521293a8f2b25c8
Mehant Kumar <mehant98@gmail.com>
2024-11-03 09:08:19 -0800

    Added missing comments in src/Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -4,12 +4,14 @@ using Microsoft.Extensions.Hosting;
 namespace ContosoCrafts.WebSite
 {
     public class Program
-    {
+    {   // Entry point of the application
         public static void Main(string[] args)
         {
+            // Builds and runs the host (server) for the web application
             CreateHostBuilder(args).Build().Run();
         }
 
+        // Configures the host builder
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
