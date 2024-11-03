759ae338fd619c2d6f17b22be010e61661527434
Mehant Kumar <mehant98@gmail.com>
2024-11-03 08:48:24 -0800

    Added missing comments in Pages/Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -5,13 +5,21 @@ namespace ContosoCrafts.WebSite.Pages
 {
     public class PrivacyModel : PageModel
     {
+        // Logger instance for logging information related to the Privacy page.
         private readonly ILogger<PrivacyModel> _logger;
-
+        
+        /// <summary>
+        /// Constructor initializing the PrivacyModel with a logger for logging purposes.
+        /// </summary>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// REST OnGet
+        /// Return all the data
+        /// </summary>
         public void OnGet()
         {
         }
