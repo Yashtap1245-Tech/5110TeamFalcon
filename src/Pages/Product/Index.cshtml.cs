f95fd5b09aebac54cc570b8dccbe3062781c7f78
Yashtap1245-Tech <yashtap1245@gmail.com>
2024-11-12 22:15:46 -0800

    Removed spaces for code formatting for Index.cshtml.cs file

--- src/Pages/Product/Index.cshtml.cs
+++ src/Pages/Product/Index.cshtml.cs
@@ -1,8 +1,5 @@
 using System.Collections.Generic;
-
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 
