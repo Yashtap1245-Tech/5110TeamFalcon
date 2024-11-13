6d59d73d5ec31e20831c5053abc9679a7c8cc65b
Vamshi-toramamidi <toramamidivamshi@gmail.com>
2024-11-12 17:20:40 -0800

    Wrote test cases for ProductTypeEnum file in productTypeEnum.Tests.cs file

--- UnitTests/Model/ProductTypeEnum.Tests.cs
+++ UnitTests/Model/ProductTypeEnum.Tests.cs
@@ -1,36 +1,87 @@
-namespace UnitTests.Model;
-
-using NUnit.Framework;
-using ContosoCrafts.WebSite.Models;
-
-[TestFixture]
-public class ProductTypeEnumTests
+namespace UnitTests.Model
 {
+    using NUnit.Framework;
+    using ContosoCrafts.WebSite.Models;
 
-    [Test]
-    public void DisplayName_Should_Return_Correct_Name_For_Collectable()
+    [TestFixture]
+    public class ProductTypeEnumTests
     {
-        // Arrange
-        var productType = ProductTypeEnum.Collectable;
+        [Test]
+        public void DisplayName_Should_Return_Correct_Name_For_Collectable()
+        {
+            // Arrange
+            var productType = ProductTypeEnum.Collectable;
 
-        // Act
-        var displayName = productType.DisplayName();
+            // Act
+            var displayName = productType.DisplayName();
 
-        // Assert
-        Assert.That(displayName, Is.EqualTo("Collectables"));
+            // Assert
+            Assert.That(displayName, Is.EqualTo("Collectables"));
+        }
+
+        [Test]
+        public void DisplayName_Should_Return_Correct_Name_For_Commercial()
+        {
+            // Arrange
+            var productType = ProductTypeEnum.Commercial;
+
+            // Act
+            var displayName = productType.DisplayName();
+
+            // Assert
+            Assert.That(displayName, Is.EqualTo("Commercial goods"));
+        }
+
+        [Test]
+        public void DisplayName_Should_Return_Correct_Name_For_Amature()
+        {
+            // Arrange
+            var productType = ProductTypeEnum.Amature;
+
+            // Act
+            var displayName = productType.DisplayName();
+
+            // Assert
+            Assert.That(displayName, Is.EqualTo("Hand Made Items"));
+        }
+
+        [Test]
+        public void DisplayName_Should_Return_Correct_Name_For_Antique()
+        {
+            // Arrange
+            var productType = ProductTypeEnum.Antique;
+
+            // Act
+            var displayName = productType.DisplayName();
+
+            // Assert
+            Assert.That(displayName, Is.EqualTo("Antiques"));
+        }
+
+        [Test]
+        public void DisplayName_Should_Return_Empty_String_For_Undefined()
+        {
+            // Arrange
+            var productType = ProductTypeEnum.Undefined;
+
+            // Act
+            var displayName = productType.DisplayName();
+
+            // Assert
+            Assert.That(displayName, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void DisplayName_Should_Return_Empty_String_For_Unknown_Value()
+        {
+            // Arrange
+            var productType = (ProductTypeEnum)999; // Invalid value outside defined enum values.
+
+            // Act
+            var displayName = productType.DisplayName();
+
+            // Assert
+            Assert.That(displayName, Is.EqualTo(""));
+        }
     }
-
-    [Test]
-    public void DisplayName_Should_Return_Correct_Name_For_Commercial()
-    {
-        // Arrange
-        var productType = ProductTypeEnum.Commercial;
-
-        // Act
-        var displayName = productType.DisplayName();
-
-        // Assert
-        Assert.That(displayName, Is.EqualTo("Commercial goods"));
-    }
-
 }
