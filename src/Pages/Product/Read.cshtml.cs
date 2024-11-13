c80b6d0050735d6b96e82732df59ea353a868bbd
Yashtap1245-Tech <yashtap1245@gmail.com>
2024-11-12 22:16:11 -0800

    Removed Unused Imports for code formatting in Read.cshtml.cs file

--- src/Pages/Product/Read.cshtml.cs
+++ src/Pages/Product/Read.cshtml.cs
@@ -1,11 +1,6 @@
-using System.Collections.Generic;
-
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
-using System.Linq;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
