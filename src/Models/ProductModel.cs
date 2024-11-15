110595bae80edcd355b4508b4bae52ac08dd5a19
Mehant Kumar <mehant98@gmail.com>
2024-11-14 19:57:09 -0800

    Made all the fields required in Models/ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -18,15 +18,20 @@ namespace ContosoCrafts.WebSite.Models
         //Url Field
         public string Url { get; set; }
         //Title Field
-        [StringLength (maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
+        [StringLength (maximumLength: 50, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
         // Director Field
+        [Required(ErrorMessage = "Director is required")]
         public string Director { get; set; }
         //Description Field
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
         //Ratings Field
+        [Required(ErrorMessage = "Ratings are required")]
         public int[] Ratings { get; set; }
         //Cast Field
+        [Required(ErrorMessage = "Cast is required")]
         public List<string> Cast { get; set; }
         public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
         //Quantity Field
@@ -38,8 +43,10 @@ namespace ContosoCrafts.WebSite.Models
         //Release Year Field
         public int ReleaseYear { get; set; }
         //YoutubeID for movie trailer
+        [Required(ErrorMessage = "YouTubeID is required")]
         public string YouTubeID { get; set; }
         //Genre field
+        [Required(ErrorMessage = "Genre is required")]
         public string Genre { get; set; }
         //IMDbRating Field
         public float IMDbRating { get; set; }
