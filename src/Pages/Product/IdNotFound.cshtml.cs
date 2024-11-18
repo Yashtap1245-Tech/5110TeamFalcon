e12fae65ce4de530f096ba7434815ebe54dc022c
Mehant Kumar <mehant98@gmail.com>
2024-11-18 01:32:23 -0800

    Added a new error page for invalid id in the url IdNotFound.cshtml.cs

---
+++ src/Pages/Product/IdNotFound.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.Product;
+
+public class IdNotFound : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
