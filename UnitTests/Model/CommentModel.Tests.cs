a2702b78eb448953239fbc2ded67b27263f8332f
Yashtap1245-Tech <yashtap1245@gmail.com>
2024-11-18 12:45:24 -0800

    Added test cases for CommentModel in CommentModel.Tests.cs file

---
+++ UnitTests/Model/CommentModel.Tests.cs
@@ -0,0 +1,56 @@
+﻿using ContosoCrafts.WebSite.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UnitTests.Model
+{
+    [TestFixture]
+    public class CommentModelTests
+    {
+        // Test case to check if ID is properly initialized with a unique GUID
+        [Test]
+        public void Should_Initialize_Id_With_Unique_Guid()
+        {
+            // Arrange & Act
+            var comment = new CommentModel();
+
+            // Assert
+            Assert.That(comment.Id, Is.Not.Null.Or.Empty);  // ID should not be null or empty
+            Assert.That(Guid.TryParse(comment.Id, out _), Is.True);  // ID should be a valid GUID
+        }
+
+        // Test case to check if the ID can be manually set
+        [Test]
+        public void Should_Set_Id_Property_Manually()
+        {
+            // Arrange
+            var comment = new CommentModel();
+            var newId = "12345678-1234-1234-1234-123456789abc";
+
+            // Act
+            comment.Id = newId;
+
+            // Assert
+            Assert.That(comment.Id, Is.EqualTo(newId));  // ID should be set to the new value
+            Assert.That(Guid.TryParse(comment.Id, out _), Is.True);  // Ensure the new ID is a valid GUID
+        }
+
+        // Test case to check if Comment can be set correctly
+        [Test]
+        public void Should_Set_Comment_Property_Correctly()
+        {
+            // Arrange
+            var commentText = "This is a test comment.";
+            var comment = new CommentModel
+            {
+                Comment = commentText
+            };
+
+            // Act & Assert
+            Assert.That(comment.Comment, Is.EqualTo(commentText));  // Comment should be correctly set
+        }
+
+    }
+}
