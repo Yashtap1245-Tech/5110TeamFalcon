a0149da60c369421c344ad06f0b22de695c86e37
Sai Sreyas Sistla <ssistla@seattleu.edu>
2024-11-19 10:27:26 -0800

    Added spaces for every concluding brace for every method in Delete.cshtml.cs file

--- src/Pages/Product/Delete.cshtml.cs
+++ src/Pages/Product/Delete.cshtml.cs
@@ -6,11 +6,17 @@ using System.Linq;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
+
+    /// <summary>
+    /// Class DeleteModel inherits the PageModel class
+    /// </summary>
     public class DeleteModel : PageModel
     {
+
         // Data Service
         public JsonFileProductService ProductService { get; }
 
+        //DeleteModel Constructor
         public DeleteModel(JsonFileProductService productService)
         {
             ProductService = productService;
@@ -20,14 +26,21 @@ namespace ContosoCrafts.WebSite.Pages.Product
         [BindProperty]
         public ProductModel Product { get; set; }
         
+        /// <summary>
+        /// OnGet Method with id as parameter
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public IActionResult OnGet(string id)
         {
+
             // Product = ProductService.GetDataForRead(id);
             Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
             if (Product == null)
             {
                 return RedirectToPage("./IdNotFound");
             }
+
             return Page();
         }
 
@@ -37,8 +50,11 @@ namespace ContosoCrafts.WebSite.Pages.Product
             {
                 return Page();
             }
+
             ProductService.DeleteData(Product.Id);
             return RedirectToPage("./Index");
         }
+
     }
+
 }
