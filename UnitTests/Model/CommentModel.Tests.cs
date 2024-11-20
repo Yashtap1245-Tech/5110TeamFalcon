956ce8b523f8b734efe79f05c658d6713f04e51e
Vamshi-toramamidi <toramamidivamshi@gmail.com>
2024-11-19 22:06:28 -0800

    Added Comments in CommentModel.Tests.cs file

--- UnitTests/Model/CommentModel.Tests.cs
+++ UnitTests/Model/CommentModel.Tests.cs
@@ -1,14 +1,14 @@
 ﻿using ContosoCrafts.WebSite.Models;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace UnitTests.Model
 {
     [TestFixture]
     public class CommentModelTests
     {
+        #region Should_Initialize_Id_With_Unique_Guid
+
         // Test case to check if ID is properly initialized with a unique GUID
         [Test]
         public void Should_Initialize_Id_With_Unique_Guid()
@@ -20,6 +20,10 @@ namespace UnitTests.Model
             Assert.That(comment.Id, Is.Not.Null.Or.Empty);  // ID should not be null or empty
             Assert.That(Guid.TryParse(comment.Id, out _), Is.True);  // ID should be a valid GUID
         }
+
+        #endregion Should_Initialize_Id_With_Unique_Guid
+
+        #region Should_Set_Id_Property_Manually
 
         // Test case to check if the ID can be manually set
         [Test]
@@ -37,6 +41,10 @@ namespace UnitTests.Model
             Assert.That(Guid.TryParse(comment.Id, out _), Is.True);  // Ensure the new ID is a valid GUID
         }
 
+        #endregion Should_Set_Id_Property_Manually
+
+        #region Should_Set_Comment_Property_Correctly
+
         // Test case to check if Comment can be set correctly
         [Test]
         public void Should_Set_Comment_Property_Correctly()
@@ -52,5 +60,6 @@ namespace UnitTests.Model
             Assert.That(comment.Comment, Is.EqualTo(commentText));  // Comment should be correctly set
         }
 
+        #endregion Should_Set_Comment_Property_Correctly
     }
 }
