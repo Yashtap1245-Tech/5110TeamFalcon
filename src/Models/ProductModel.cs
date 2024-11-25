7c9f3fc0a2aff82080bd64c003a749f026dac82e
Vamshi-toramamidi <toramamidivamshi@gmail.com>
2024-11-25 14:11:04 -0800

    Fixed these product model sot that it works with the test cases in Prodcut.Model cs file

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -10,15 +10,15 @@ namespace ContosoCrafts.WebSite.Models
     /// </summary>
     public class ProductModel
     {
-        //ID Field
+        // ID Field
         public string Id { get; set; }
 
-        //Image Field
+        // Image Field
         [JsonPropertyName("img")]
         public string Image { get; set; }
-        
-        //Title Field
-        [StringLength (maximumLength: 50, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}.")]
+
+        // Title Field
+        [StringLength(maximumLength: 50, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}.")]
         [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
 
@@ -27,39 +27,40 @@ namespace ContosoCrafts.WebSite.Models
         [Required(ErrorMessage = "Director is required")]
         public string Director { get; set; }
 
-        //Description Field
-        [StringLength(maximumLength: 500, MinimumLength = 1, ErrorMessage = "The description should not been more than 300 words.")]
+        // Description Field
+        [StringLength(maximumLength: 500, MinimumLength = 1, ErrorMessage = "The description should not been more than 500 characters.")]
         [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
-        
-        //Ratings Field
+
+        // Ratings Field
         public int[] Ratings { get; set; }
-        
-        //Cast Field
+
+        // Cast Field
         [Required(ErrorMessage = "Cast is required")]
-        public List<string> Cast { get; set; }
+        [MinLength(1, ErrorMessage = "At least one cast member is required.")]
+        public List<string> Cast { get; set; } = new List<string>();
+
+        // Product Type
         public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
 
-        //Release Year Field
+        // Release Year Field
         [Range(1000, 9999, ErrorMessage = "The year must be a four-digit number.")]
         public int ReleaseYear { get; set; }
-        
-        //YoutubeID for movie trailer
+
+        // YouTubeID for movie trailer
         [Required(ErrorMessage = "YouTubeID is required")]
         public string YouTubeID { get; set; }
 
-        //Genre field
-        [StringLength(maximumLength: 30, MinimumLength = 1, ErrorMessage = "The description should not been more than 300 words.")]
+        // Genre field
+        [StringLength(maximumLength: 30, MinimumLength = 1, ErrorMessage = "The genre should have a length between 1 and 30 characters.")]
         [Required(ErrorMessage = "Genre is required")]
         public string Genre { get; set; }
-        
-        //IMDbRating Field
+
+        // IMDb Rating Field
         public float IMDbRating { get; set; }
 
         // Store the Comments entered by the users on this product
         [MaxLength(30, ErrorMessage = "Comment must be 30 characters or less.")]
         public List<string> CommentList { get; set; } = new List<string>();
-
     }
-
 }
