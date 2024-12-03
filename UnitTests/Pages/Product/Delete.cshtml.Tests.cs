9ff7bcb1c01f781e323ae6fe34ecdabee57b858f
Mehant Kumar <mehant98@gmail.com>
2024-12-03 14:42:27 -0800

    Added test for Genre Enum in Delte.cshmtl.Tests.cs

--- UnitTests/Pages/Product/Delete.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Delete.cshtml.Tests.cs
@@ -97,7 +97,7 @@ namespace UnitTests.Pages.Product.Delete
             Assert.That(PageModel.Product.Title, Is.EqualTo("The Shawshank Redemption"));
             Assert.That(PageModel.Product.Director, Is.EqualTo("Frank Darabont"));
             Assert.That(PageModel.Product.Description, Is.EqualTo("The Shawshank Redemption is a 1994 American prison drama film written and directed by Frank Darabont, based on the 1982 Stephen King novella Rita Hayworth and Shawshank Redemption. The film tells the story of banker Andy Dufresne (Tim Robbins), who is sentenced to life in Shawshank State Penitentiary for the murders of his wife and her lover, despite his claims of innocence. Over the following two decades, he befriends a fellow prisoner, contraband smuggler Ellis Red Redding (Morgan Freeman), and becomes instrumental in a money laundering operation led by the prison warden Samuel Norton (Bob Gunton). William Sadler, Clancy Brown, Gil Bellows, and James Whitmore appear in supporting roles."));
-            Assert.That(PageModel.Product.Genre, Is.EqualTo("Drama"));
+            Assert.That(PageModel.Product.Genre, Is.EqualTo(GenreEnum.Drama));
         }
 
         #endregion OnGet Tests
